e2b88feae3519e6c060c2c0aea144d987cdd97d2
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-09-28 20:21:20 -0300

    agregado del paquete principal.controladores y de la clase "ControladorPrincipal"

---
+++ src/principal/controladores/ControladorPrincipal.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal.controladores;
+
+import java.util.ArrayList;
+import autores.modelos.Alumno;
+import autores.modelos.Profesor;
+import grupos.modelos.Grupo;
+import idiomas.modelos.Idioma;
+import lugares.modelos.Lugar;
+import palabrasclaves.modelos.PalabraClave;
+import tipos.modelos.Tipo;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class ControladorPrincipal {
+    public static void main(String[] args) {
+        Alumno unAlumno;
+        Profesor unProfesor;
+        Grupo unGrupo;
+        Idioma unIdioma;
+        Lugar unLugar;
+        PalabraClave unaPalabraClave;
+        Tipo unTipo;
+        
+        
+        unAlumno = new Alumno("Agustin", "Ramos", "ecyce", 41041592, "1601159");
+        Alumno unAlumno2 = new Alumno ("Pepi", "Ramirez", "cacatua", 13131322, "1601160");
+        
+        unProfesor = new Profesor ("Esteban", "Quito", "ecece", 44444444, "TP fisica 1");
+        Profesor unProfesor2 = new Profesor ("Gustavo", "Savio", "clave", 23232323, "Teorias Química");
+        
+        unGrupo = new Grupo ("Los pi", "lo + mejore");
+        Grupo unGrupo2 = new Grupo ("epitafio", "no se que significa");
+        
+        unIdioma = new Idioma ("Ingles");
+        Idioma unIdioma2 = new Idioma ("Japones");
+        
+        unLugar = new Lugar ("el baño... que se yo");
+        Lugar unLugar2 = new Lugar ("Decanato");
+        
+        unaPalabraClave = new PalabraClave ("sanguchito de miga");
+        PalabraClave unaPalabraClave2 = new PalabraClave ("Mango");
+        
+        unTipo = new Tipo ("no se para que son los tipos pero acá estamos xd");
+        Tipo unTipo2 = new Tipo ("ehh no se, tipo de helado.... palito de agua xd");
+        
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        ArrayList<Profesor> listaProfesores = new ArrayList<>();
+        ArrayList<Grupo> listaGrupos = new ArrayList<>();
+        ArrayList<Idioma> listaIdiomas = new ArrayList<>();
+        ArrayList<Lugar> listalugares = new ArrayList<>();
+        ArrayList<PalabraClave> listaPalabrasClave = new ArrayList<>();
+        ArrayList<Tipo> listaTipos = new ArrayList<>();
+        
+        listaAlumnos.add(unAlumno);
+        listaAlumnos.add(unAlumno2);
+        
+        listaProfesores.add(unProfesor);
+        listaProfesores.add(unProfesor2);
+        
+        listaGrupos.add(unGrupo);
+        listaGrupos.add(unGrupo2);
+        
+        listaIdiomas.add(unIdioma);
+        listaIdiomas.add(unIdioma2);
+        
+        listalugares.add(unLugar);
+        listalugares.add(unLugar2);
+        
+        listaPalabrasClave.add(unaPalabraClave);
+        listaPalabrasClave.add(unaPalabraClave2);
+        
+        listaTipos.add(unTipo);
+        listaTipos.add(unTipo2);
+        
+        System.out.println("\n---ALUMNOS--- ");
+        for (Alumno e : listaAlumnos){
+            e.mostrar();
+        }
+        System.out.println("\n---PROFESORES--- ");
+        for (Profesor f : listaProfesores){
+            f.mostrar();
+        }
+        System.out.println("\n---GRUPOS--- ");
+        for (Grupo g : listaGrupos){
+            g.mostrar();
+        }
+        System.out.println("\n---IDIOMAS--- ");
+        for (Idioma i : listaIdiomas){
+            System.out.println(i.toString());
+        }
+        System.out.println("\n---LUGARES--- ");
+        for (Lugar l : listalugares){
+            System.out.println(l.toString());
+        }
+        System.out.println("\n---PALABRAS CLAVE--- ");
+        for (PalabraClave p : listaPalabrasClave){
+            System.out.println(p.toString());
+        }
+        System.out.println("\n---TIPOS--- ");
+        for (Tipo t : listaTipos){
+            System.out.println(t.toString());
+        }
+    }
+}
