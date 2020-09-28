7f9942e55ea5b87bbe3c6456b2514c84cf788ae4
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-09-28 20:24:45 -0300

    comentado de lo hecho para el agregado de ventanas y guardandolo para el TP3

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -20,92 +20,92 @@ import tipos.modelos.Tipo;
  */
 public class ControladorPrincipal {
     public static void main(String[] args) {
-        Alumno unAlumno;
-        Profesor unProfesor;
-        Grupo unGrupo;
-        Idioma unIdioma;
-        Lugar unLugar;
-        PalabraClave unaPalabraClave;
-        Tipo unTipo;
-        
-        
-        unAlumno = new Alumno("Agustin", "Ramos", "ecyce", 41041592, "1601159");
-        Alumno unAlumno2 = new Alumno ("Pepi", "Ramirez", "cacatua", 13131322, "1601160");
-        
-        unProfesor = new Profesor ("Esteban", "Quito", "ecece", 44444444, "TP fisica 1");
-        Profesor unProfesor2 = new Profesor ("Gustavo", "Savio", "clave", 23232323, "Teorias Química");
-        
-        unGrupo = new Grupo ("Los pi", "lo + mejore");
-        Grupo unGrupo2 = new Grupo ("epitafio", "no se que significa");
-        
-        unIdioma = new Idioma ("Ingles");
-        Idioma unIdioma2 = new Idioma ("Japones");
-        
-        unLugar = new Lugar ("el baño... que se yo");
-        Lugar unLugar2 = new Lugar ("Decanato");
-        
-        unaPalabraClave = new PalabraClave ("sanguchito de miga");
-        PalabraClave unaPalabraClave2 = new PalabraClave ("Mango");
-        
-        unTipo = new Tipo ("no se para que son los tipos pero acá estamos xd");
-        Tipo unTipo2 = new Tipo ("ehh no se, tipo de helado.... palito de agua xd");
-        
-        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
-        ArrayList<Profesor> listaProfesores = new ArrayList<>();
-        ArrayList<Grupo> listaGrupos = new ArrayList<>();
-        ArrayList<Idioma> listaIdiomas = new ArrayList<>();
-        ArrayList<Lugar> listalugares = new ArrayList<>();
-        ArrayList<PalabraClave> listaPalabrasClave = new ArrayList<>();
-        ArrayList<Tipo> listaTipos = new ArrayList<>();
-        
-        listaAlumnos.add(unAlumno);
-        listaAlumnos.add(unAlumno2);
-        
-        listaProfesores.add(unProfesor);
-        listaProfesores.add(unProfesor2);
-        
-        listaGrupos.add(unGrupo);
-        listaGrupos.add(unGrupo2);
-        
-        listaIdiomas.add(unIdioma);
-        listaIdiomas.add(unIdioma2);
-        
-        listalugares.add(unLugar);
-        listalugares.add(unLugar2);
-        
-        listaPalabrasClave.add(unaPalabraClave);
-        listaPalabrasClave.add(unaPalabraClave2);
-        
-        listaTipos.add(unTipo);
-        listaTipos.add(unTipo2);
-        
-        System.out.println("\n---ALUMNOS--- ");
-        for (Alumno e : listaAlumnos){
-            e.mostrar();
-        }
-        System.out.println("\n---PROFESORES--- ");
-        for (Profesor f : listaProfesores){
-            f.mostrar();
-        }
-        System.out.println("\n---GRUPOS--- ");
-        for (Grupo g : listaGrupos){
-            g.mostrar();
-        }
-        System.out.println("\n---IDIOMAS--- ");
-        for (Idioma i : listaIdiomas){
-            System.out.println(i.toString());
-        }
-        System.out.println("\n---LUGARES--- ");
-        for (Lugar l : listalugares){
-            System.out.println(l.toString());
-        }
-        System.out.println("\n---PALABRAS CLAVE--- ");
-        for (PalabraClave p : listaPalabrasClave){
-            System.out.println(p.toString());
-        }
-        System.out.println("\n---TIPOS--- ");
-        for (Tipo t : listaTipos){
-            System.out.println(t.toString());
-        }
+//        Alumno unAlumno;
+//        Profesor unProfesor;
+//        Grupo unGrupo;
+//        Idioma unIdioma;
+//        Lugar unLugar;
+//        PalabraClave unaPalabraClave;
+//        Tipo unTipo;
+//        
+//        
+//        unAlumno = new Alumno("Agustin", "Ramos", "ecyce", 41041592, "1601159");
+//        Alumno unAlumno2 = new Alumno ("Pepi", "Ramirez", "cacatua", 13131322, "1601160");
+//        
+//        unProfesor = new Profesor ("Esteban", "Quito", "ecece", 44444444, "TP fisica 1");
+//        Profesor unProfesor2 = new Profesor ("Gustavo", "Savio", "clave", 23232323, "Teorias Química");
+//        
+//        unGrupo = new Grupo ("Los pi", "lo + mejore");
+//        Grupo unGrupo2 = new Grupo ("epitafio", "no se que significa");
+//        
+//        unIdioma = new Idioma ("Ingles");
+//        Idioma unIdioma2 = new Idioma ("Japones");
+//        
+//        unLugar = new Lugar ("el baño... que se yo");
+//        Lugar unLugar2 = new Lugar ("Decanato");
+//        
+//        unaPalabraClave = new PalabraClave ("sanguchito de miga");
+//        PalabraClave unaPalabraClave2 = new PalabraClave ("Mango");
+//        
+//        unTipo = new Tipo ("no se para que son los tipos pero acá estamos xd");
+//        Tipo unTipo2 = new Tipo ("ehh no se, tipo de helado.... palito de agua xd");
+//        
+//        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+//        ArrayList<Profesor> listaProfesores = new ArrayList<>();
+//        ArrayList<Grupo> listaGrupos = new ArrayList<>();
+//        ArrayList<Idioma> listaIdiomas = new ArrayList<>();
+//        ArrayList<Lugar> listalugares = new ArrayList<>();
+//        ArrayList<PalabraClave> listaPalabrasClave = new ArrayList<>();
+//        ArrayList<Tipo> listaTipos = new ArrayList<>();
+//        
+//        listaAlumnos.add(unAlumno);
+//        listaAlumnos.add(unAlumno2);
+//        
+//        listaProfesores.add(unProfesor);
+//        listaProfesores.add(unProfesor2);
+//        
+//        listaGrupos.add(unGrupo);
+//        listaGrupos.add(unGrupo2);
+//        
+//        listaIdiomas.add(unIdioma);
+//        listaIdiomas.add(unIdioma2);
+//        
+//        listalugares.add(unLugar);
+//        listalugares.add(unLugar2);
+//        
+//        listaPalabrasClave.add(unaPalabraClave);
+//        listaPalabrasClave.add(unaPalabraClave2);
+//        
+//        listaTipos.add(unTipo);
+//        listaTipos.add(unTipo2);
+//        
+//        System.out.println("\n---ALUMNOS--- ");
+//        for (Alumno e : listaAlumnos){
+//            e.mostrar();
+//        }
+//        System.out.println("\n---PROFESORES--- ");
+//        for (Profesor f : listaProfesores){
+//            f.mostrar();
+//        }
+//        System.out.println("\n---GRUPOS--- ");
+//        for (Grupo g : listaGrupos){
+//            g.mostrar();
+//        }
+//        System.out.println("\n---IDIOMAS--- ");
+//        for (Idioma i : listaIdiomas){
+//            System.out.println(i.toString());
+//        }
+//        System.out.println("\n---LUGARES--- ");
+//        for (Lugar l : listalugares){
+//            System.out.println(l.toString());
+//        }
+//        System.out.println("\n---PALABRAS CLAVE--- ");
+//        for (PalabraClave p : listaPalabrasClave){
+//            System.out.println(p.toString());
+//        }
+//        System.out.println("\n---TIPOS--- ");
+//        for (Tipo t : listaTipos){
+//            System.out.println(t.toString());
+//        }
     }
 }
