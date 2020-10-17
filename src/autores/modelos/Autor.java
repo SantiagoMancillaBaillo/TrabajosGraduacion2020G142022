37ca78e4cacd1921ae27cc7b40fece0d79968ade
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-17 06:57:20 -0300

    Agregadas importaciones para trabajar con "MiembrosEnGrupo" y su respectivo ArrayList, comentado un constructor sin uso, agregado de métodos "agregarGrupo()" y "mostrarGrupo()" todavía falta terminar. Agregado de "equals" y "hashcode" para evitar que coincidan los documentos de los autores.

--- src/autores/modelos/Autor.java
+++ src/autores/modelos/Autor.java
@@ -5,15 +5,20 @@
  */
 package autores.modelos;
 
+//import grupos.modelos.Grupo;
+import grupos.modelos.MiembroEnGrupo;
+import java.util.ArrayList;
+
 /**
  *
  * @author Windows 10
  */
-public class Autor {
+public abstract class Autor {
     private String nombres;
     private String apellidos;
     private String clave;
     private int dni;
+    private ArrayList<MiembroEnGrupo> grupoMiembro;
 
     public Autor(String nombres, String apellidos, String clave, int dni) {
         this.nombres = nombres;
@@ -21,6 +26,16 @@ public class Autor {
         this.clave = clave;
         this.dni = dni;
     }
+
+//    public Autor(String nombres, String apellidos, String clave, int dni, ArrayList<MiembroEnGrupo> grupoMiembro) {
+//        this.nombres = nombres;
+//        this.apellidos = apellidos;
+//        this.clave = clave;
+//        this.dni = dni;
+//        this.grupoMiembro = grupoMiembro;
+//    }
+    
+    
 
     public String getNombres() {
         return this.nombres;
@@ -54,10 +69,48 @@ public class Autor {
         this.dni = dni;
     }
     
-    public void mostrar(){
-        System.out.println("\nAlumno: " + this.apellidos.toUpperCase() + ", " + this.nombres);
-        System.out.println("DNI: " + this.dni);
+    public void agregarGrupo(MiembroEnGrupo m){
+        grupoMiembro.add(m);
     }
+    
+    public void mostrarGrupo(){
+        for(MiembroEnGrupo i : grupoMiembro){
+            i.mostrar("t");
+        }
+            
+    }
+    
+    public void mostrar(){
+        System.out.println("Autor: " + this.apellidos.toUpperCase() + ", " + this.nombres);
+        System.out.println("DNI: " + this.dni);
+        System.out.println("Miembro del grupo: " + this.grupoMiembro);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.dni;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Autor other = (Autor) obj;
+        if (this.dni != other.dni) {
+            return false;
+        }
+        return true;
+    }
+
     
     
 }
