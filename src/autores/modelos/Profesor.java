9bf9ee3092d75c620b256d9138a492cf3fa52385
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-17 06:49:27 -0300

    cambios en la clase "Profesor" al pasar a ser subclase de la clase "Autor"

--- src/autores/modelos/Profesor.java
+++ src/autores/modelos/Profesor.java
@@ -19,10 +19,10 @@ public class Profesor extends Autor {
 //    private String nombres;
 //    private String clave
     private Cargo cargo;
-    private ArrayList<MiembroEnGrupo> gruposMiembro;
+//    private ArrayList<MiembroEnGrupo> gruposMiembro;
     
-    public Profesor (String nombres, String apellidos, int dni, String clave, Cargo cargo){
-            super(nombres, apellidos, clave, dni);
+    public Profesor (String nombres, String apellidos, int dni, String clave, Cargo cargo){//, ArrayList<MiembroEnGrupo> grupoMiembro){
+            super(nombres, apellidos, clave, dni);//, grupoMiembro);
             this.cargo = cargo;
     }
 //    public String verNombres(){
@@ -56,18 +56,19 @@ public class Profesor extends Autor {
         this.cargo = cargo;
     }
 
-    public ArrayList<MiembroEnGrupo> getGruposMiembro() {
-        return gruposMiembro;
-    }
-
-    public void setGruposMiembro(ArrayList<MiembroEnGrupo> gruposMiembro) {
-        this.gruposMiembro = gruposMiembro;
-    }
+//    public ArrayList<MiembroEnGrupo> getGruposMiembro() {
+//        return gruposMiembro;
+//    }
+//
+//    public void setGruposMiembro(ArrayList<MiembroEnGrupo> gruposMiembro) {
+//        this.gruposMiembro = gruposMiembro;
     
     @Override
     public void mostrar(){
+        System.out.println("....Profesor....");
         super.mostrar();
         System.out.println("Cargo: " + cargo);
+        System.out.println("................");
     }
     public void mostrar(int x){
         super.mostrar();
