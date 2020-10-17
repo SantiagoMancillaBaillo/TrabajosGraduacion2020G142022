52ed34fa55f2dcadc807d5d6a83355e2749b01db
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-17 00:21:50 -0300

    agregado de objetos, un constructor, sus respectivos métodos get y set, y el método "mostrar()"

--- src/autores/modelos/Autor.java
+++ src/autores/modelos/Autor.java
@@ -10,5 +10,54 @@ package autores.modelos;
  * @author Windows 10
  */
 public class Autor {
+    private String nombres;
+    private String apellidos;
+    private String clave;
+    private int dni;
+
+    public Autor(String nombres, String apellidos, String clave, int dni) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.clave = clave;
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return this.nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return this.apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getClave() {
+        return this.clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getDni() {
+        return this.dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    
+    public void mostrar(){
+        System.out.println("\nAlumno: " + this.apellidos.toUpperCase() + ", " + this.nombres);
+        System.out.println("DNI: " + this.dni);
+    }
+    
     
 }
