0f9c8e93d2fc63dc42f860ab4e3389e65e22a092
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-17 06:58:09 -0300

    Agregado de "equals" y "hashcode" para evitar que coincidan los cx entre los alumnos (también sirve para autores).

--- src/autores/modelos/Alumno.java
+++ src/autores/modelos/Alumno.java
@@ -5,6 +5,8 @@
  */
 package autores.modelos;
 
+import java.util.Objects;
+
 /**
  *
  * @author Windows 10
@@ -50,11 +52,40 @@ public class Alumno extends Autor {
     public void asignarCx (String cx){
         this.cx = cx;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.cx);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        if (!Objects.equals(this.cx, other.cx)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
     @Override
     public void mostrar(){
+        System.out.println("....Alumno....");
         super.mostrar();
         System.out.println("CX: " + this.cx);
+        System.out.println("..............");
     }
     
 }
