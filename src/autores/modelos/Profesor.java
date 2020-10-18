9f41d14f34f4d4c8836406766b59e39693c5206e
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-18 00:12:38 -0300

    cambio de método mostrar en clase "Profesor" de la clase autor por "mostrar(int x,int y, int z)"

--- src/autores/modelos/Profesor.java
+++ src/autores/modelos/Profesor.java
@@ -66,7 +66,7 @@ public class Profesor extends Autor {
     @Override
     public void mostrar(){
         System.out.println("....Profesor....");
-        super.mostrar();
+        super.mostrar(1,1,1);
         System.out.println("Cargo: " + cargo);
         System.out.println("................");
     }
