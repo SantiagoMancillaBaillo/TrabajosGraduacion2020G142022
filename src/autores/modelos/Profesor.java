48ce425c30cfe6869995f229790598900dc240a0
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-19 01:51:46 -0300

    Se eliminaron las partes comentadas sin uso de la clase "profesor"
    Se cambió como muestra los datos el método "mostrar"

--- src/autores/modelos/Profesor.java
+++ src/autores/modelos/Profesor.java
@@ -19,36 +19,17 @@ public class Profesor extends Autor {
 //    private String nombres;
 //    private String clave
     private Cargo cargo;
-//    private ArrayList<MiembroEnGrupo> gruposMiembro;
+    private ArrayList<MiembroEnGrupo> grupoMiembro = new ArrayList<>();
     
-    public Profesor (String nombres, String apellidos, int dni, String clave, Cargo cargo){//, ArrayList<MiembroEnGrupo> grupoMiembro){
-            super(nombres, apellidos, clave, dni);//, grupoMiembro);
+    public Profesor (String nombres, String apellidos, int dni, String clave, Cargo cargo){
+            super(nombres, apellidos, clave, dni);
             this.cargo = cargo;
     }
-//    public String verNombres(){
-//        return nombres;
-//    }
-//    public void asignarNombres(String nombres){
-//        this.nombres = nombres;
-//    }
-//    public String verApellidos(){
-//        return apellidos;
-//    }
-//    public void asignarApellidos(String apellidos){
-//        this.apellidos = apellidos;
-//    }
-//    public String verClave(){
-//        return clave;
-//    }
-//    public void asignarClave(String clave){
-//        this.clave = clave;
-//    }
-//    public int verDni(){
-//        return dni;
-//    }
-//    public void asignarDni(int dni){
-//        this.dni = dni;
-//    }
+
+    public Profesor(String nombres, String apellidos, String clave, int dni) {
+        super(nombres, apellidos, clave, dni);
+    }
+
     public Cargo verCargo(){
         return cargo;
     }
@@ -56,21 +37,14 @@ public class Profesor extends Autor {
         this.cargo = cargo;
     }
 
-//    public ArrayList<MiembroEnGrupo> getGruposMiembro() {
-//        return gruposMiembro;
-//    }
-//
-//    public void setGruposMiembro(ArrayList<MiembroEnGrupo> gruposMiembro) {
-//        this.gruposMiembro = gruposMiembro;
-    
+//[nro_documento] Apellido, Nombres por ejemplo: [30759167] Gimenez, María José
     @Override
-    public void mostrar(){
+    public void mostrar(/*String u*/){
         System.out.println("....Profesor....");
+        System.out.println("Autor: ["+ super.getDni()+"] " + super.getApellidos().toUpperCase() + ", " + super.getNombres());
+//        System.out.println("DNI: " + super.getDni());
+        System.out.println("Cargo: " + this.verCargo());
         super.mostrar(1,1,1);
-        System.out.println("Cargo: " + cargo);
-        System.out.println("................");
-    }
-    public void mostrar(int x){
-        super.mostrar();
+        System.out.println("..............");
     }
 }
