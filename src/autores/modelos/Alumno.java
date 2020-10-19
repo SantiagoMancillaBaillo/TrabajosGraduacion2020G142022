1c6a30b3bb1a3d9b26da1f346227bc1dda6a76de
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-10-19 02:39:53 -0300

    - se agregaron importaciones
    - se agregó un ArrayList para trabajar con objetos del tipo "MiembroEnGrupo"
    
    - se eliminaron partes comentadas sin uso
    
    - se modificó el método "mostrar()" para que este funcione de acuerdo al enunciado al momento de redefinir en método "mostrar()" de la clase "Autor"

--- src/autores/modelos/Alumno.java
+++ src/autores/modelos/Alumno.java
@@ -5,6 +5,8 @@
  */
 package autores.modelos;
 
+import grupos.modelos.MiembroEnGrupo;
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -14,38 +16,16 @@ import java.util.Objects;
 public class Alumno extends Autor {
     private String cx; 
     
+    private ArrayList<MiembroEnGrupo> grupoMiembro = new ArrayList<>();
+    
     public Alumno(String nombres, String apellidos, String clave, int dni, String cx){
             super(nombres, apellidos, clave, dni);
-//            this.nombres = nombres;
-//            this.apellidos = apellidos;
-//            this.dni = dni;
-//            this.clave = clave;
             this.cx = cx;
     }
-//    public String verNombres(){
-//        return nombres;
-//    }
-//    public void asignarNombres(String nombres){
-//        this.nombres = nombres;
-//    }
-//    public String verApellidos(){
-//        return apellidos;
-//    }
-//    public void asignarApellidos(String apellidos){
-//        this.apellidos = apellidos;
-//    }
-//    public String verClave(){
-//        return clave;
-//    }
-//    public void asignarClave(String clave){
-//        this.clave = clave;
-//    }
-//    public int verDni(){
-//        return dni;
-//    }
-//    public void asignarDni(int dni){
-//        this.dni = dni;
-//    }
+    public Alumno (String nombres, String apellidos, String clave, int dni){
+        super(nombres, apellidos, clave, dni);
+    }
+    
     public String verCx(){
         return cx;
     }
@@ -82,15 +62,14 @@ public class Alumno extends Autor {
         }
         return true;
     }
-    
-    
-    
-    
+     
     @Override
-    public void mostrar(){
+    public void mostrar(/*String u*/){
         System.out.println("....Alumno....");
-        super.mostrar(1,1,1);
+        System.out.println("Autor: ["+ super.getDni()+"] " + super.getApellidos().toUpperCase() + ", " + super.getNombres());
+//        System.out.println("DNI: " + super.getDni());
         System.out.println("CX: " + this.cx);
+        super.mostrar(1,1,1);
         System.out.println("..............");
     }
     
