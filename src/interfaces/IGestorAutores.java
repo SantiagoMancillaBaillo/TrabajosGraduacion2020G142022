61ebd2ee2c0e51e38986073d376ea4f8c0d54852
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-13 20:50:47 -0300

    creación de interfaces de gestores

---
+++ src/interfaces/IGestorAutores.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import autores.modelos.Alumno;
+import autores.modelos.Autor;
+import autores.modelos.Profesor;
+import autores.modelos.Cargo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Windows 10
+ */
+public interface IGestorAutores {
+    public static final String AMENSAJE_EXITO = "Se creó el autor correctamente";
+    public static final String AMENSAJE_REPETIDO = "No se creó el autor. Motivo: ya existe uno con ese dni";
+    public static final String AMENSAJE_ERROR = "No se creó el autor. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    
+    public String nuevoAutor(int dni, String apellidos, String nombres, Cargo cargo, String clave, String claveRepetida);
+    public String nuevoAutor(int dni, String apellidos, String nombres, String cx, String clave, String claveRepetida);
+    public String modificarAutor(Autor autor, String apellidos, String nombres, Cargo cargo, String clave, String claveRepetida);
+    public String modificarAutor(Autor autor, String apellidos, String nombres, String cx, String clave, String claveRepetida);
+    public boolean existeEsteAutor(Autor autor);
+    public ArrayList<Autor> verAutores();
+    public ArrayList<Profesor> verProfesores();
+    public ArrayList<Alumno> verAlumnos();
+    public Autor verAutor(int dni);
+}
