6264776f59098ea6fa47d5237bfa24ee7ba34094
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-22 06:35:32 -0300

    Agregados nuevos mensajes para la modificación de un autor

--- src/interfaces/IGestorAutores.java
+++ src/interfaces/IGestorAutores.java
@@ -19,6 +19,9 @@ public interface IGestorAutores {
     public static final String AMENSAJE_EXITO = "Se creó el autor correctamente";
     public static final String AMENSAJE_REPETIDO = "No se creó el autor. Motivo: ya existe uno con ese dni";
     public static final String AMENSAJE_ERROR = "No se creó el autor. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    public static final String AmodMENSAJE_EXITO = "Se modificó el Autor correctamente";
+    public static final String AodMENSAJE_ERROR = "No se pudo modificar el Autor";
+    public static final String AmodMENSAJE_NO_EXISTE = "No se pudo modificar el autor, pues éste no existe";
     
     public String nuevoAutor(int dni, String apellidos, String nombres, Cargo cargo, String clave, String claveRepetida);
     public String nuevoAutor(int dni, String apellidos, String nombres, String cx, String clave, String claveRepetida);
