edb63f6ce784eb85dd9988b19b5977985c51e96f
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-23 04:01:28 -0300

    Se modificó el main para comprobar que los gestores funcionan correctamente. Hasta ahora no hubo problemas. Se crearon Autores, Grupos, Tipos, Lugares, Idiomas, Palabras Clave y Publicaciones comprobando que no se añadan duplicados.
    Se agregó un Grupo a un Autor para ver si el Autor tambien se agregaba al Grupo. Funcionó correctamente.
    
    FIN TP5

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -8,20 +8,26 @@ package principal.controladores;
 import autores.modelos.Autor;
 import autores.modelos.Cargo;
 import autores.modelos.GestorAutores;
+import grupos.modelos.GestorGrupos;
 import grupos.modelos.Grupo;
+import grupos.modelos.MiembroEnGrupo;
 import grupos.modelos.Rol;
 import idiomas.modelos.GestorIdiomas;
 import idiomas.modelos.Idioma;
 import interfaces.IGestorAutores;
+import interfaces.IGestorGrupos;
 import interfaces.IGestorIdiomas;
 import interfaces.IGestorLugares;
 import interfaces.IGestorPalabrasClaves;
+import interfaces.IGestorPublicaciones;
 import interfaces.IGestorTipos;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import lugares.modelos.GestorLugares;
 import lugares.modelos.Lugar;
 import palabrasclaves.modelos.GestorPalabrasClaves;
 import palabrasclaves.modelos.PalabraClave;
+import publicaciones.modelos.GestorPublicaciones;
 import publicaciones.modelos.Publicacion;
 import tipos.modelos.GestorTipos;
 import tipos.modelos.Tipo;
@@ -47,28 +53,28 @@ public class ControladorPrincipal {
         
         System.out.println("------------------GRUPOS------------------");
         
-        Grupo grupo1 = new Grupo("Grupo 1", "Descripción 1");
-        Grupo grupo2 = new Grupo("Grupo 2", "Descripción 2");
-        Grupo grupo3 = new Grupo("Grupo 3", "Descripción 3");
-        Grupo grupo4 = new Grupo("Grupo 4", "Descripción 4");
-        Grupo grupo5 = new Grupo("Grupo 5", "Descripción 5");
-        Grupo grupo6 = new Grupo("Grupo 1", "Descripción 5"); //nombre repetido
-        
-        if (!grupos.contains(grupo1))
-            grupos.add(grupo1);
-        if (!grupos.contains(grupo2))
-            grupos.add(grupo2);
-        if (!grupos.contains(grupo3))
-            grupos.add(grupo3);
-        if (!grupos.contains(grupo4))
-            grupos.add(grupo4);
-        if (!grupos.contains(grupo5))
-            grupos.add(grupo5);
-        if (!grupos.contains(grupo6))
-            grupos.add(grupo6);
-        
-        for(Grupo g : grupos)
-            g.mostrar();
+//        Grupo grupo1 = new Grupo("Grupo 1", "Descripción 1");
+//        Grupo grupo2 = new Grupo("Grupo 2", "Descripción 2");
+//        Grupo grupo3 = new Grupo("Grupo 3", "Descripción 3");
+//        Grupo grupo4 = new Grupo("Grupo 4", "Descripción 4");
+//        Grupo grupo5 = new Grupo("Grupo 5", "Descripción 5");
+//        Grupo grupo6 = new Grupo("Grupo 1", "Descripción 5"); //nombre repetido
+//        
+//        if (!grupos.contains(grupo1))
+//            grupos.add(grupo1);
+//        if (!grupos.contains(grupo2))
+//            grupos.add(grupo2);
+//        if (!grupos.contains(grupo3))
+//            grupos.add(grupo3);
+//        if (!grupos.contains(grupo4))
+//            grupos.add(grupo4);
+//        if (!grupos.contains(grupo5))
+//            grupos.add(grupo5);
+//        if (!grupos.contains(grupo6))
+//            grupos.add(grupo6);
+//        
+//        for(Grupo g : grupos)
+//            g.mostrar();
         
 //        System.out.println("------------------AUTORES------------------");
         IGestorAutores autor = GestorAutores.crear();
@@ -76,6 +82,8 @@ public class ControladorPrincipal {
         IGestorIdiomas idioma = GestorIdiomas.crear();
         IGestorTipos tipo = GestorTipos.crear();
         IGestorPalabrasClaves palabraClave = GestorPalabrasClaves.crear();
+        IGestorGrupos grupo = GestorGrupos.crear();
+        IGestorPublicaciones publicacion = GestorPublicaciones.crear();
         
         System.out.println("------------------INGRESO DE AUTORES------------------");
         System.out.println(autor.nuevoAutor(10, "Apellido10", "Nombre10", Cargo.TITULAR, "Clave10", "Clave10"));
@@ -90,8 +98,8 @@ public class ControladorPrincipal {
         System.out.println("------------------------------------------------------");
         
         System.out.println("------------------INGRESO DE IDIOMAS------------------");
-        System.out.println(idioma.nuevoIdioma("Ingles"));
-        System.out.println(idioma.nuevoIdioma("Ingles"));//REPETIDO
+        System.out.println(idioma.nuevoIdioma("ingles"));
+        System.out.println(idioma.nuevoIdioma("ingles"));//REPETIDO
         System.out.println(idioma.nuevoIdioma("español"));
         System.out.println(idioma.nuevoIdioma(""));//VACIO
         System.out.println(idioma.nuevoIdioma(null));//NULO
@@ -119,7 +127,35 @@ public class ControladorPrincipal {
         System.out.println(palabraClave.nuevaPalabraClave("pc2"));
         System.out.println(palabraClave.nuevaPalabraClave(""));//VACIO
         System.out.println(palabraClave.nuevaPalabraClave(null));//NULO
+        ArrayList<PalabraClave> palabrasclave1 = new ArrayList<>();
+        ArrayList<PalabraClave> palabrasclave2 = new ArrayList<>();
+        palabrasclave1.add(palabraClave.verPalabraClave("pc1"));
+        palabrasclave1.add(palabraClave.verPalabraClave("pc2"));
+        palabrasclave2.add(palabraClave.verPalabraClave("pc2"));
         System.out.println("------------------------------------------------------");
+        
+        System.out.println("------------------INGRESO DE GRUPOS------------------");
+        System.out.println(grupo.nuevoGrupo("grupo1", "descricpion1"));
+        System.out.println(grupo.nuevoGrupo("grupo1", "descricpion1"));//REPETIDO
+        System.out.println(grupo.nuevoGrupo("grupo2", "descricpion2"));
+        System.out.println(grupo.nuevoGrupo("Super Administradores", "Grupo para los super administradores"));
+        System.out.println(grupo.nuevoGrupo("", ""));//VACIO
+        System.out.println(grupo.nuevoGrupo(null, null));//NULO
+        System.out.println("-----------------------------------------------------");
+        
+        System.out.println("------------------INGRESO DE PUBLICACIONES------------------");
+        MiembroEnGrupo mg1 = new MiembroEnGrupo(autor.verAutor(10), grupo.verGrupo("grupo1"), Rol.COLABORADOR);
+        MiembroEnGrupo mg2 = new MiembroEnGrupo(autor.verAutor(1), grupo.verGrupo("grupo2"), Rol.ADMINISTRADOR);
+        LocalDate fecha1= LocalDate.of(2020, 06, 24);
+        LocalDate fecha2= LocalDate.of(2020, 06, 26);
+        LocalDate fecha3= LocalDate.of(2023, 05, 20);
+        
+        System.out.println(publicacion.nuevaPublicacion("publicacion1", mg1, fecha1, tipo.verTipo("tipo1"), idioma.verIdioma("ingles"), lugar.verLugar("lugar1"), palabrasclave1, "enlace1", "resumen1"));
+        System.out.println(publicacion.nuevaPublicacion("publicacion1", mg1, fecha1, tipo.verTipo("tipo1"), idioma.verIdioma("ingles"), lugar.verLugar("lugar1"), palabrasclave1, "enlace1", "resumen1"));//REPETIDO
+        System.out.println(publicacion.nuevaPublicacion("publicacion2", mg2, fecha2, tipo.verTipo("tipo2"), idioma.verIdioma("español"), lugar.verLugar("lugar2"), palabrasclave2, "enlace2", "resumen2"));
+        System.out.println(publicacion.nuevaPublicacion("", mg1, fecha3, tipo.verTipo("tipo2"), idioma.verIdioma("español"), lugar.verLugar("lugar1"), palabrasclave1, "", ""));//VACIO
+        System.out.println(publicacion.nuevaPublicacion(null, null, null, null, null, null, null, null, null));//NULO
+        System.out.println("-----------------------------------------------------");
         
         System.out.println("------------------AUTORES------------------");
         autor.verAutores();
@@ -136,10 +172,17 @@ public class ControladorPrincipal {
         
         palabraClave.verPalabrasClaves();
         
+        grupo.verGrupos();
+        
+        publicacion.verPublicaciones();
+        
         idioma.verIdioma("español");
         lugar.verLugar("lugar1");
         tipo.verTipo("tipo1");
         palabraClave.verPalabraClave("pc2");
+        grupo.verGrupo("grupo1");
+        publicacion.verPublicacion("publicacion1");
+        
         
         
         autor.verAutor(10);
@@ -234,7 +277,7 @@ public class ControladorPrincipal {
         ya no es miembro del grupo.
         */
 //        grupo1.quitarMiembro(profesor1);
-        grupo1.mostrar();
+//        grupo1.mostrar();
 
         /*
         Crear un nuevo grupo para los super administradores. 
@@ -243,21 +286,18 @@ public class ControladorPrincipal {
         con el rol de colaborador, verificando que se lo
         agrega pero con el rol de administrador.
         */
-        Grupo grupo7 = new Grupo("Super Administradores", "Grupo para los super administradores"); //grupo para los super administradores
-        if (!grupos.contains(grupo7))
-            grupos.add(grupo7);
 //        grupo7.agregarMiembro(profesor1, Rol.COLABORADOR);
-        autor.verAutor(1).agregarGrupo(grupo7, Rol.COLABORADOR);
+        autor.verAutor(1).agregarGrupo(grupo.verGrupo("Super Administradores"), Rol.COLABORADOR);
         autor.verAutor(1);
-        grupo7.mostrar();
+        grupo.verGrupo("Super Administradores").mostrar();
 
         /*
         Verificar que el último grupo creado es de super administradores
         y cualquiera de los otros grupos no 
         (usar el método esSuperAdministradores() definido en la clase Grupo).
         */
-        System.out.println(grupo7.esSuperAdministradores());
-        System.out.println(grupo1.esSuperAdministradores());
+        System.out.println(grupo.verGrupo("Super Administradores").esSuperAdministradores());
+//        System.out.println(grupo1.esSuperAdministradores());
         
         /*Tomar el autor que se agregó al grupo de super administradores, 
         verificar que el mismo es super administrador 
