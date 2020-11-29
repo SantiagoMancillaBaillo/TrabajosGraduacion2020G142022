30a021890e4c2d5a781f13e8b07416192716a2ab
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:48:14 -0300

    -se agregaron los métodos "borrarPalabraClave", "buscarPalabrasClaves", y "existeEstaPalabraClave"
    -se agregaron mensajes de error,exito y en caso de no existir para el método "borrarPalabraClave"
    
    -se cambió el método "verPalabrasClaves" para que devuelva una Lista y no un ArrayList

--- src/interfaces/IGestorPalabrasClaves.java
+++ src/interfaces/IGestorPalabrasClaves.java
@@ -5,7 +5,8 @@
  */
 package interfaces;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.List;
 import palabrasclaves.modelos.PalabraClave;
 
 /**
@@ -16,7 +17,13 @@ public interface IGestorPalabrasClaves {
     public static final String PMENSAJE_EXITO = "Se creó la Palabra Clave correctamente";
     public static final String PMENSAJE_REPETIDO = "No se creó la Palabra Clave. Motivo: ya existe una con ese nombre";
     public static final String PMENSAJE_ERROR = "No se creó la Palabra Clave. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    public static final String PborrarMENSAJE_EXITO = "Se borró la Palabra Clave correctamente";
+    public static final String PborrarMENSAJE_ERROR = "No se borró la Palabra Clave pues hay una publicación que contiene esta palabra clave.";
+    public static final String PborrarMENSAJE_NO_EXISTE = "No se borró la Palabra Clave, no hay palabras clave con ese nombre";
     public String nuevaPalabraClave(String nombre);
-    public ArrayList<PalabraClave> verPalabrasClaves();
+    public List<PalabraClave> verPalabrasClaves();
     public PalabraClave verPalabraClave(String nombre);
-}
+    public String borrarPalabraClave(PalabraClave palabraClave);
+    public List<PalabraClave> buscarPalabrasClaves(String nombre);
+    public boolean existeEstaPalabraClave(PalabraClave palabraClave);
+    }
