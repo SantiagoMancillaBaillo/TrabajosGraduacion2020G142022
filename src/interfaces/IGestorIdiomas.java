f4a3a218a1167c71d9062fc86edb17548b16d63f
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:24:21 -0300

    -se agregaron los métodos "borrarIdioma", "buscarIdiomas", y "existeEsteIdioma"
    -se agregaron mensajes de error,exito y en caso de no existir para el método "borrarIdioma"
    
    -se cambió el método "verIdiomas" para que devuelva una Lista y no un ArrayList

--- src/interfaces/IGestorIdiomas.java
+++ src/interfaces/IGestorIdiomas.java
@@ -6,7 +6,7 @@
 package interfaces;
 
 import idiomas.modelos.Idioma;
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,7 +16,13 @@ public interface IGestorIdiomas {
     public static final String IMENSAJE_EXITO = "Se añadió el ididoma correctamente";
     public static final String IMENSAJE_REPETIDO = "No se añadió el idioma. Motivo: ya existe";
     public static final String IMENSAJE_ERROR = "No se añadió el idioma. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    public static final String IborrarMENSAJE_EXITO = "Se eliminó el Idioma correctamente";
+    public static final String IborrarMENSAJE_ERROR = "No se borró el Idioma pues hay una publicación que lo contiene.";
+    public static final String IborrarMENSAJE_NO_EXISTE = "No hay Idioma con ese nombre";
     public String nuevoIdioma(String nombre);
-    public ArrayList<Idioma> verIdiomas();
+    public List<Idioma> verIdiomas();
     public Idioma verIdioma(String nombre);
+    public String borrarIdioma(Idioma idioma);
+    public List<Idioma> buscarIdiomas(String nombre);
+    public boolean existeEsteIdioma(Idioma idioma);
 }
