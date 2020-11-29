9d14cfb77cbae8bb28e1e33d2a7e7875c21aeafb
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:31:48 -0300

    -se agregaron las importaciones necesarias.
    -se agregaron los métodos "borrarIdioma", "buscarIdiomas", y "existeEsteIdioma"
    
    -se cambió el método "verIdiomas" para que devuelva una Lista y no un ArrayList
    
    -se agregó una nueva lista para mostrar solo los Idiomas que coinciden con el nombre en método "buscarIdioma"
    
    -en "verIdiomas" se agregó el método sort para que ordene la lista por orden alfabético
    
    -el método "borrarIdioma" instancia al gestor de publicaciones para utilzar el método "hayPublicacionesConEsteIdioma" para verificar si hay publicaciones que lo contienen o no para ser borrado y utiliza el método "existeEsteIdioma" para comprobar que el idioma que se ingresa exista.
    
    -el método "buscarIdiomas" utiliza la lista "idiomas2" para agregar en ella los idiomas que coinciden con el ingresado (total o parcialmente).
    
    -el método "existeEsteIdioma" verifica que el idioma exista y devuelve true en caso afirmativo.

--- src/idiomas/modelos/GestorIdiomas.java
+++ src/idiomas/modelos/GestorIdiomas.java
@@ -6,14 +6,19 @@
 package idiomas.modelos;
 
 import interfaces.IGestorIdiomas;
+import interfaces.IGestorPublicaciones;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import publicaciones.modelos.GestorPublicaciones;
 
 /**
  *
  * @author Windows 10
  */
 public class GestorIdiomas implements IGestorIdiomas{
-    private ArrayList<Idioma> idiomas = new ArrayList<>();
+    private List<Idioma> idiomas = new ArrayList<>();
+    private List<Idioma> idiomas2 = new ArrayList<>();
     private static GestorIdiomas instanciaIdiomas;
 
     public GestorIdiomas() {
@@ -46,8 +51,9 @@ public class GestorIdiomas implements IGestorIdiomas{
     }
 
     @Override
-    public ArrayList<Idioma> verIdiomas() {
+    public List<Idioma> verIdiomas() {
         System.out.println("--------------IDIOMAS--------------");
+        Collections.sort(idiomas);
         for (Idioma i : idiomas){
             System.out.println(i);
         }
@@ -59,12 +65,61 @@ public class GestorIdiomas implements IGestorIdiomas{
         Idioma id = new Idioma(nombre);
         for(Idioma i : idiomas){
             if(id.verIdioma().equals(i.verIdioma())){
-                System.out.println("IDIOMA ENCONTRADO");
-                System.out.println(i);
+//                System.out.println("IDIOMA ENCONTRADO");
+//                System.out.println(i);
                 return i;
             }
         }
         return null;
     }
+
+    @Override
+    public String borrarIdioma(Idioma idioma) {
+        IGestorPublicaciones gp = GestorPublicaciones.crear();
+        if(existeEsteIdioma(idioma) == true && (gp.hayPublicacionesConEsteIdioma(idioma) == true)){
+                return IborrarMENSAJE_ERROR;
+        }
+        if(existeEsteIdioma(idioma) == true && (gp.hayPublicacionesConEsteIdioma(idioma) == false)){
+            this.idiomas.remove(idioma);
+            return IborrarMENSAJE_EXITO;
+        }
+        else
+            return IborrarMENSAJE_NO_EXISTE;
+    }
+
+    @Override
+    public List<Idioma> buscarIdiomas(String nombre) {
+        this.idiomas2.clear();
+        if(nombre == null || nombre.isBlank()){
+            System.out.println("No se puede buscar. No se proporcionó ningun nombre");
+        }
+        else{
+            for(Idioma i : idiomas){
+                if(i.verIdioma().contains(nombre)){
+                    this.idiomas2.add(i);
+                }
+            }
+            Collections.sort(this.idiomas2);
+            for(Idioma i : idiomas2){
+                i.mostrar();
+            }
+            
+        }
+        return idiomas2;
+    }
+
+    @Override
+    public boolean existeEsteIdioma(Idioma idioma) {
+        boolean existe  = false;
+        if(idiomas.contains(idioma)){
+            existe = true;
+        }
+        else{
+            existe = false;
+        }
+        return existe;
+    }
+    
+    
     
 }
