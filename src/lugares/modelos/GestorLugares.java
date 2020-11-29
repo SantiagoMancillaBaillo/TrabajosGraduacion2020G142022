381a5dff05cefaee451157269fab7ec11d4ff6f1
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:45:33 -0300

    -se agregaron las importaciones necesarias.
    -se agregaron los métodos "borrarLugar", "buscarLugares", y "existeEsteLugar"
    
    -se cambió el método "verLugares" para que devuelva una Lista y no un ArrayList
    
    -se agregó una nueva lista para mostrar solo los Lugares que coinciden con el nombre ingresado en método "buscarLugares"
    
    -en "verLugares" se agregó el método sort para que ordene la lista por orden alfabético
    
    -el método "borrarLugar" instancia al gestor de publicaciones para utilzar el método "hayPublicacionesConEsteLugar" para verificar si hay publicaciones que lo contienen o no para ser borrado y utiliza el método "existeEsteLugar" para comprobar que el idioma que se ingresa exista.
    
    -el método "buscarLugares" utiliza la lista "lugares2" para agregar en ella los lugares que coinciden con el ingresado (total o parcialmente).
    
    -el método "existeEsteLugar" verifica que el lugar exista y devuelve true en caso afirmativo.

--- src/lugares/modelos/GestorLugares.java
+++ src/lugares/modelos/GestorLugares.java
@@ -6,14 +6,19 @@
 package lugares.modelos;
 
 import interfaces.IGestorLugares;
+import interfaces.IGestorPublicaciones;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import publicaciones.modelos.GestorPublicaciones;
 
 /**
  *
  * @author Windows 10
  */
 public class GestorLugares implements IGestorLugares{
-    private ArrayList<Lugar> lugares = new ArrayList<>();
+    private List<Lugar> lugares = new ArrayList<>();
+    private List<Lugar> lugares2 = new ArrayList<>();
     private static GestorLugares instanciaLugares;
 
     public GestorLugares() {
@@ -43,8 +48,9 @@ public class GestorLugares implements IGestorLugares{
     }
 
     @Override
-    public ArrayList<Lugar> verLugares() {
+    public List<Lugar> verLugares() {
         System.out.println("--------------LUGARES--------------");
+        Collections.sort(lugares);
         for (Lugar l : lugares){
             System.out.println(l);
         }
@@ -56,12 +62,63 @@ public class GestorLugares implements IGestorLugares{
         Lugar lu = new Lugar(nombre);
         for(Lugar l : lugares){
             if(lu.verLugar().equals(l.verLugar())){
-                System.out.println("LUGAR ENCONTRADO");
-                System.out.println(l);
+//                System.out.println("LUGAR ENCONTRADO");
+//                System.out.println(l);
                 return l;
             }
         }
         return null;
     }
+
+    @Override
+    public String borrarLugar(Lugar lugar) {
+        IGestorPublicaciones gp = GestorPublicaciones.crear();
+        if(existeEsteLugar(lugar) == true && (gp.hayPublicacionesConEsteLugar(lugar) == true)){
+                return LborrarMENSAJE_ERROR;
+        }
+        if(existeEsteLugar(lugar) == true && (gp.hayPublicacionesConEsteLugar(lugar) == false)){
+            this.lugares.remove(lugar);
+            return LborrarMENSAJE_EXITO;
+        }
+        else
+            return LborrarMENSAJE_NO_EXISTE;
+    }
+
+    @Override
+    public List<Lugar> buscarLugares(String nombre) {
+        this.lugares2.clear();
+        if(nombre == null || nombre.isBlank()){
+            System.out.println("No se puede buscar. No se proporcionó ningun nombre");
+        }
+        else{
+            for(Lugar l : lugares){
+                if(l.verLugar().contains(nombre)){
+                    lugares2.add(l);
+                }
+                else{
+                    System.out.println("No se encontró Palabra Clave con ese nombre");
+                }
+            }
+            Collections.sort(lugares2);
+            for(Lugar l : lugares2){
+                System.out.println(l);
+            }
+        }
+        return lugares2;
+    }
+
+    @Override
+    public boolean existeEsteLugar(Lugar lugar) {
+        boolean existe = false;
+        if(this.lugares.contains(lugar)){
+            existe = true;
+        }
+        else{
+            existe = false;
+        }
+        return existe;
+    }
+    
+    
     
 }
