a66b3ee8177eeabdc3573502e4a93b16f63a28b5
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:42:08 -0300

    -se agregaron los métodos "borrarLugar", "buscarLugares", y "existeEsteLugar"
    -se agregaron mensajes de error,exito y en caso de no existir para el método "borrarLugar"
    
    -se cambió el método "verLugares" para que devuelva una Lista y no un ArrayList

--- src/interfaces/IGestorLugares.java
+++ src/interfaces/IGestorLugares.java
@@ -5,7 +5,7 @@
  */
 package interfaces;
 
-import java.util.ArrayList;
+import java.util.List;
 import lugares.modelos.Lugar;
 
 /**
@@ -16,7 +16,13 @@ public interface IGestorLugares {
     public static final String LMENSAJE_EXITO = "Se creó el Lugar correctamente";
     public static final String LMENSAJE_REPETIDO = "No se creó el Lugar. Motivo: ya existe uno con ese nombre";
     public static final String LMENSAJE_ERROR = "No se creó el Lugar. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    public static final String LborrarMENSAJE_EXITO = "Se borró el Lugar correctamente";
+    public static final String LborrarMENSAJE_ERROR = "No se borró el Lugar pues hay una publicación que contiene esta Lugar.";
+    public static final String LborrarMENSAJE_NO_EXISTE = "No se borró el Lugar, no hay Lugar con ese nombre";
     public String nuevoLugar(String nombre);
-    public ArrayList<Lugar> verLugares();
+    public List<Lugar> verLugares();
     public Lugar verLugar(String nombre);
+    public String borrarLugar(Lugar lugar);
+    public List<Lugar> buscarLugares(String nombre);
+    public boolean existeEsteLugar(Lugar lugar);
 }
