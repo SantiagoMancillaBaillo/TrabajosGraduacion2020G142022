84fa3ca8dd985fe3be5ab6aa493d699a860f31a9
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:52:55 -0300

    -se agregaron las importaciones necesarias.
    -se agregaron los métodos "borrarPalabraClave", "buscarPalabrasClaves", y "existeEstaPalabraClave"
    
    -se cambió el método "verPalabrasClaves" para que devuelva una Lista y no un ArrayList
    
    -se agregó una nueva lista para mostrar solo las Palabras Clave que coinciden con el nombre ingresado en método "buscarPalabrasClaves"
    
    -en "verPalabrasClaves" se agregó el método sort para que ordene la lista por orden alfabético
    
    -el método "borrarPalabraClave" instancia al gestor de publicaciones para utilzar el método "hayPublicacionesConEstaPalabraClave" para verificar si hay publicaciones que la contienen o no para ser borrada y utiliza el método "existeEstaPalabraClave" para comprobar que la palabra clave que se ingresa exista.
    
    -el método "buscarPalabrasClaves" utiliza la lista "palabrasClaves2" para agregar en ella las palabras clave que coinciden con la ingresada (total o parcialmente).
    
    -el método "existeEstaPalabraClave" verifica que la palabra clave exista y devuelve true en caso afirmativo.

--- src/palabrasclaves/modelos/GestorPalabrasClaves.java
+++ src/palabrasclaves/modelos/GestorPalabrasClaves.java
@@ -6,14 +6,19 @@
 package palabrasclaves.modelos;
 
 import interfaces.IGestorPalabrasClaves;
+import interfaces.IGestorPublicaciones;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import publicaciones.modelos.GestorPublicaciones;
 
 /**
  *
  * @author Windows 10
  */
 public class GestorPalabrasClaves implements IGestorPalabrasClaves{
-    private ArrayList<PalabraClave> palabrasClaves = new ArrayList<>();
+    private List<PalabraClave> palabrasClaves = new ArrayList<>();
+    private List<PalabraClave> palabrasClaves2 = new ArrayList<>();
     private static GestorPalabrasClaves instanciaPalabrasClaves;
 
 
@@ -44,8 +49,9 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves{
     }
 
     @Override
-    public ArrayList<PalabraClave> verPalabrasClaves() {
+    public List<PalabraClave> verPalabrasClaves() {
         System.out.println("--------------PALABRAS CLAVE--------------");
+        Collections.sort(palabrasClaves);
         for (PalabraClave pc : palabrasClaves){
             System.out.println(pc);
         }
@@ -57,12 +63,63 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves{
         PalabraClave palc = new PalabraClave(nombre);
         for(PalabraClave pc : palabrasClaves){
             if(palc.verPalabraClave().equals(pc.verPalabraClave())){
-                System.out.println("PALABRA CLAVE ENCONTRADA");
-                System.out.println(pc);
+//                System.out.println("PALABRA CLAVE ENCONTRADA");
+//                System.out.println(pc);
                 return pc;
             }
         }
         return null;
     }
+
+    @Override
+    public String borrarPalabraClave(PalabraClave palabraClave) {
+        IGestorPublicaciones gp = GestorPublicaciones.crear();
+        if(existeEstaPalabraClave(palabraClave) == true && (gp.hayPublicacionesConEstaPalabraClave(palabraClave) == true)){
+                return PborrarMENSAJE_ERROR;
+        }
+        if(existeEstaPalabraClave(palabraClave) == true && (gp.hayPublicacionesConEstaPalabraClave(palabraClave) == false)){
+            this.palabrasClaves.remove(palabraClave);
+            return PborrarMENSAJE_EXITO;
+        }
+        else
+            return PborrarMENSAJE_NO_EXISTE;
+    }
+
+    @Override
+    public List<PalabraClave> buscarPalabrasClaves(String nombre) {
+        this.palabrasClaves2.clear();
+        if(nombre == null || nombre.isBlank()){
+            System.out.println("No se puede buscar. No se proporcionó ningun nombre");
+        }
+        else{
+            for(PalabraClave pc : palabrasClaves){
+                if(pc.verPalabraClave().contains(nombre)){
+                    palabrasClaves2.add(pc);
+                }
+                else{
+                    System.out.println("No se encontró Palabra Clave con ese nombre");
+                }
+            }
+            Collections.sort(palabrasClaves2);
+            for(PalabraClave pc : palabrasClaves2){
+                System.out.println(pc);
+            }
+        }
+        return palabrasClaves2;
+    }
+
+    @Override
+    public boolean existeEstaPalabraClave(PalabraClave palabraClave) {
+        boolean existe = false;
+        if(palabrasClaves.contains(palabraClave)){
+            existe = true;
+        }
+        else{
+            existe = false;
+        }
+        return existe;
+    }
+    
+    
     
 }
