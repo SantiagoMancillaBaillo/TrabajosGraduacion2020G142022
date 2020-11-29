9958c42cea1a4db93df9d1bb1e1ea5bb965515ec
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 06:08:58 -0300

    -se agregaron los métodos "borrarAutor", "buscarProfesores", "buscarAlumnos", "hayAutoresConEsteGrupo".
    -se agregaron mensajes de error,exito y en caso de no existir para el método "borrarAutor"
    
    -se cambió el método "verAutores", "verAlumnos" y "verProfesores" para que devuelvan una Lista y no un ArrayList

--- src/interfaces/IGestorAutores.java
+++ src/interfaces/IGestorAutores.java
@@ -9,7 +9,8 @@ import autores.modelos.Alumno;
 import autores.modelos.Autor;
 import autores.modelos.Profesor;
 import autores.modelos.Cargo;
-import java.util.ArrayList;
+import grupos.modelos.Grupo;
+import java.util.List;
 
 /**
  *
@@ -22,14 +23,22 @@ public interface IGestorAutores {
     public static final String AmodMENSAJE_EXITO = "Se modificó el Autor correctamente";
     public static final String AodMENSAJE_ERROR = "No se pudo modificar el Autor";
     public static final String AmodMENSAJE_NO_EXISTE = "No se pudo modificar el autor, pues éste no existe";
+    public static final String AborrarMENSAJE_EXITO = "Se eliminó el Autor correctamente";
+    public static final String AborrarMENSAJE_ERROR = "No se borró el Autor pues hay una publicación que lo contiene.";
+    public static final String AborrarMENSAJE_NO_EXISTE = "No existe ese Autor";
     
     public String nuevoAutor(int dni, String apellidos, String nombres, Cargo cargo, String clave, String claveRepetida);
     public String nuevoAutor(int dni, String apellidos, String nombres, String cx, String clave, String claveRepetida);
     public String modificarAutor(Autor autor, String apellidos, String nombres, Cargo cargo, String clave, String claveRepetida);
     public String modificarAutor(Autor autor, String apellidos, String nombres, String cx, String clave, String claveRepetida);
     public boolean existeEsteAutor(Autor autor);
-    public ArrayList<Autor> verAutores();
-    public ArrayList<Profesor> verProfesores();
-    public ArrayList<Alumno> verAlumnos();
+    public List<Autor> verAutores();
+    public List<Profesor> verProfesores();
+    public List<Alumno> verAlumnos();
     public Autor verAutor(int dni);
+    
+    public String borrarAutor(Autor autor);
+    public List<Profesor> buscarProfesores(String apellidos);
+    public List<Alumno> buscarAlumnos(String apellidos);
+    public boolean hayAutoresConEsteGrupo(Grupo grupo);
 }
