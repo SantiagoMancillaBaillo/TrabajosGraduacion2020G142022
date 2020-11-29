9a9cf0e30ea317f1caf4425169482f4141c13895
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-11-29 05:56:34 -0300

    -se agregaron los métodos "borrarGrupo" y "buscarGrupo"
    -se agregaron mensajes de error,exito y en caso de no existir para el método "borrarGrupo"
    
    -se cambió el método "verGrupos" para que devuelva una Lista y no un ArrayList

--- src/interfaces/IGestorGrupos.java
+++ src/interfaces/IGestorGrupos.java
@@ -7,6 +7,7 @@ package interfaces;
 
 import grupos.modelos.Grupo;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,9 +20,14 @@ public interface IGestorGrupos {
     public static final String GmodMENSAJE_EXITO = "Se modificó el Grupo correctamente";
     public static final String GmodMENSAJE_NO_EXISTE = "No se modificó el Grupo pues este no existe";
     public static final String GmodMENSAJE_ERROR = "No se modificó el Grupo. Motivo: datos incorrectos (No se pueden agregar datos vacios o nulos)";
+    public static final String GborrarMENSAJE_EXITO = "Se borró el Grupo correctamente";
+    public static final String GborrarMENSAJE_ERROR = "No se borró el Grupo pues hay un Autor que pertenece a este Grupo. Elimine el Autor primero para eliminar el grupo";
+    public static final String GborrarMENSAJE_NO_EXISTE = "No se encontró ningun Grupo coincidente";
     public String nuevoGrupo(String nombre, String descripcion);
     public String modificarGrupo(Grupo grupo, String descripcion);
-    public ArrayList<Grupo> verGrupos();
+    public List<Grupo> verGrupos();
     public Grupo verGrupo(String nombre);
     public boolean existeEsteGrupo(Grupo grupo);
+    public String borrarGrupo(Grupo grupo);
+    public List<Grupo> buscarGrupos(String nombre);
 }
