5396233b40b85168dc956d029812a47fd2cb2b9c
SantiMancillaBaillo <santibaillo@hotmail.com>
2020-12-10 05:23:06 -0300

    -Se agregó el modelo para el ComboBox de Grupos, el método devuelve un String porque de lo contrario mostraba la id del Grupo por el ComboBox, luego de haber seleccionado el Grupo se toma el String y se busca el grupo que tiene el mismo nombre utilizando el gestor y el método "verGrupo(String nombre)"

---
+++ src/grupos/modelos/ModeloComboGrupos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupos.modelos;
+
+import interfaces.IGestorGrupos;
+import javax.swing.DefaultComboBoxModel;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class ModeloComboGrupos extends DefaultComboBoxModel {
+
+    public ModeloComboGrupos() {
+        IGestorGrupos gg = GestorGrupos.crear();
+        for(Grupo grupo : gg.verGrupos())
+            this.addElement(grupo.verNombre());
+    }
+    
+    public String obtenerGrupo(){
+        return (String)this.getSelectedItem();
+    }
+    
+    public void seleccionarIdioma(Grupo grupo){
+        this.setSelectedItem(grupo);
+    }
+    
+}
